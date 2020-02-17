0adb18f07b9783e78e9beb44101e355420bd275c
Xabertooth <46043902+Xabertooth@users.noreply.github.com>
2020-02-17 17:03:42 +0500

    Add files via upload

---
+++ ch10_task_points.cpp
@@ -0,0 +1,61 @@
+#include "std_lib_facilities.h"
+#include <fstream>
+
+struct Point{
+	int x;
+	int y;
+};
+
+istream& operator>>(istream& is, Point& p)
+{
+	is >> p.x >> p.y ;
+}
+
+void print_point(Point p){
+	cout << '\n' << '(' << p.x << ';' << p.y << ')' << '\n';
+}
+
+void out_to_file(string oname){
+	cout << "Имя файла для записи: " << oname << '\n';
+	ofstream ost{oname};
+	if(!ost) error("Невозможно открыть выходной файл!");
+	
+	cout << "Введите семь пар значений x, y!" << '\n';
+	Point pnt;
+	vector<Point> original_points(0);
+	for (int i = 0; i<6; ++i){
+		cout << "Пара значений " << i+1 << ':' << '\t';
+		cin >> pnt;
+		original_points.push_back(pnt);
+	}
+	
+	for(Point p : original_points) 
+		print_point(p);	
+	
+	for(Point p : original_points) 
+		ost << '(' << p.x << ';' << p.y << ")\n";
+}
+
+void in_from_file(string iname){
+	cout << "Имя файла для чтения: " << iname << '\n';
+	ifstream ist{iname};
+	if(!ist) error("Невозможно открыть входной файл!");
+	
+	vector<Point>processed_points(0);
+	
+	Point p;
+	char ch1, ch2, ch3;
+	while(ist){
+		ist >> ch1 >> p.x >> ch2 >> p.y >> ch3;
+		processed_points.push_back(p);
+	}
+	
+	for(Point p : processed_points) 
+		print_point(p);		
+}
+
+int main(){
+	out_to_file("mydata.txt");
+	in_from_file("mydata.txt");
+	
+}
